b0dbab279394bbd013c51866882f7bc00d9f7ad5
GunSmoke12 <me_most-wanted_12@hotmail.com>
2016-01-19 23:14:44 +0000

    Initial commit. Most of index page is setup already. Mainly content and text need to be added.

---
+++ js/jscript.js
@@ -0,0 +1,18 @@
+$(document).ready( function () {
+    var classAdded = false;
+    
+    $(window).scroll(function (event) {
+        if ($(window).scrollTop() > 12) {
+            $('header').addClass('headerActive');
+        } else {
+            $('header').removeClass('headerActive');
+        }
+    });
+    
+    $('nav a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - $('header').outerHeight()
+        }, 1000);
+        return false;
+    });
+});
